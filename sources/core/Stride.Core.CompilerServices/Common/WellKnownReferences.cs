a955e1fcdbf59043c2ce76884c11b167e1611bae
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-10-01 19:44:31 +0200

    feat: reduce to internal scope

--- sources/core/Stride.Core.CompilerServices/Common/WellKnownReferences.cs
+++ sources/core/Stride.Core.CompilerServices/Common/WellKnownReferences.cs
@@ -1,42 +1,43 @@
 namespace Stride.Core.CompilerServices.Common;
 internal static class WellKnownReferences
 {
-    public static INamedTypeSymbol? DataMemberAttribute(Compilation compilation)
+    internal static INamedTypeSymbol? DataMemberAttribute(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName("Stride.Core.DataMemberAttribute");
     }
 
-    public static INamedTypeSymbol? IDictionary_generic(Compilation compilation)
+    internal static INamedTypeSymbol? IDictionary_generic(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName(typeof(IDictionary<,>).FullName);
     }
 
-    public static INamedTypeSymbol? DataMemberIgnoreAttribute(Compilation compilation)
+    internal static INamedTypeSymbol? DataMemberIgnoreAttribute(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName("Stride.Core.DataMemberIgnoreAttribute");
     }
 
-    public static INamedTypeSymbol? DataMemberMode(Compilation compilation)
+    internal static INamedTypeSymbol? DataMemberMode(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName("Stride.Core.DataMemberMode");
     }
 
-    public static INamedTypeSymbol? DataMemberUpdatableAttribute(Compilation compilation)
+    internal static INamedTypeSymbol? DataMemberUpdatableAttribute(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName("Stride.Updater.DataMemberUpdatableAttribute");
     }
 
-    public static INamedTypeSymbol? DataContractAttribute(Compilation compilation)
+    internal static INamedTypeSymbol? DataContractAttribute(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName("Stride.Core.DataContractAttribute");
     }
 
-    public static INamedTypeSymbol? ModuleInitializerAttribute(Compilation compilation)
+    internal static INamedTypeSymbol? ModuleInitializerAttribute(Compilation compilation)
     {
         return compilation.GetTypeByMetadataName(ModuleInitializerAttributeName);
     }
-    public const string ModuleInitializerAttributeName = "Stride.Core.ModuleInitializerAttribute";
+    internal const string ModuleInitializerAttributeName = "Stride.Core.ModuleInitializerAttribute";
     public static bool HasAttribute(this ISymbol symbol, INamedTypeSymbol attribute)
+    internal static bool HasAttribute(this ISymbol symbol, INamedTypeSymbol attribute)
     {
         if (symbol.GetAttributes().Any(attr => attr.AttributeClass?.OriginalDefinition.Equals(attribute, SymbolEqualityComparer.Default) ?? false))
         {
