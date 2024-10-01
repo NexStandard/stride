803c19f223c32030c2a56d68302bb8094db751a3
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-10-01 19:48:08 +0200

    add link docs

--- sources/core/Stride.Core.CompilerServices/Common/DiagnosticCategory.cs
+++ sources/core/Stride.Core.CompilerServices/Common/DiagnosticCategory.cs
@@ -2,5 +2,9 @@ namespace Stride.Core.CompilerServices.Common;
 public static class DiagnosticCategory
 {
     public const string Serialization = "Serialization";
+    /// <summary>
+    /// This constant defines the URL format for accessing specific roslyn diagnostic documentation pages. 
+    /// The format string includes a placeholder <c>{0}</c> which should be replaced with the specific diagnostic ID or identifier when generating the URL.
+    /// </summary>
     public const string LinkFormat = "https://doc.stride3d.net/latest/en/diagnostics/{0}.html";
 }
